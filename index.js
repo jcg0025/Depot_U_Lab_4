05821ccce6ae6c58b9ed6d847fcf1f03e146fd51
Jacob Gossett <jgossett121@gmail.com>
2016-02-25 19:12:35 -0600

    dbl click ready

--- index.js
+++ index.js
@@ -14,11 +14,23 @@ var addBlock = function () {
         var block = document.createElement('div');
             block.className = 'daBlocks'; 
             block.setAttribute('id', blockArray.length.toString())
-            block.innerText = block.id;    
-            document.body.appendChild(block);
-
+            block.innerText = block.id;   
+            document.body.appendChild(block);    
             block.addEventListener('click', function() {
                 block.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
             });
-}
+        
+            block.addEventListener('dblclick', function(){
+                
+            });        
+}       
 
+
+ 
+            //     var numID = parseInt(block.id, 10);
+            // if (numID % 2 == 0) {
+            //     numID += 1;
+            //     var unluckyID = numID.toString();
+            //     console.log(unluckyID);    
+            
+     
